ce984b3f26486793e4dca89818912699604466c3
GitHub Action <action@github.com>
2025-09-30 07:56:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/angel5215/neonglowjetbrains/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/angel5215/neonglowjetbrains/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.angel5215.neonglowjetbrains.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.angel5215.neonglowjetbrains.MyBundle
+import com.github.angel5215.neonglowjetbrains.services.MyProjectService
 import javax.swing.JButton
 
 
